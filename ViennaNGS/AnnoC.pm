9a8995d58b394337476488cb24df1164dd1469bc
Michael T. Wolfinger <michael@wolfinger.eu>
2013-09-04 11:25:49 +0200

    added GPL3 Copyright notice

--- ViennaNGS/AnnoC.pm
+++ ViennaNGS/AnnoC.pm
@@ -1,3 +1,31 @@
+#!/usr/bin/perl -w
+# -*-CPerl-*-
+# Last changed Time-stamp: <2013-09-04 11:18:52 mtw>
+#
+#
+# ***********************************************************************
+# *  Copyright notice
+# *
+# *  Copyright 2013 Michael Thomas Wolfinger <michael@wolfinger.eu>
+# *  All rights reserved
+# *
+# *  This program is free software: you can redistribute it and/or modify
+# *  it under the terms of the GNU General Public License as published by
+# *  the Free Software Foundation, either version 3 of the License, or
+# *  (at your option) any later version.
+# *
+# *  This program is distributed in the hope that it will be useful,
+# *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+# *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# *  GNU General Public License for more details.
+# *
+# *  You should have received a copy of the GNU General Public License
+# *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# *
+# *  This copyright notice MUST APPEAR in all copies of the script!
+# ***********************************************************************
+#
+
 package ViennaNGS::AnnoC;
 
 use Exporter;
@@ -5,7 +33,6 @@ use strict;
 use warnings;
 use Data::Dumper;
 use Bio::Tools::GFF;
-
 
 our @ISA       = qw(Exporter);
 our $VERSION   = '0.01';
