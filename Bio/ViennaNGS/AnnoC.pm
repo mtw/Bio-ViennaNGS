efb517c7c7e0e10f1457f4cf21d63781c9751da2
Michael T. Wolfinger <michael@wolfinger.eu>
2014-10-04 01:15:42 +0200

    added features2bed(); added test for bedtools; updated POD

--- Bio/ViennaNGS/AnnoC.pm
+++ Bio/ViennaNGS/AnnoC.pm
@@ -1,5 +1,5 @@
 # -*-CPerl-*-
-# Last changed Time-stamp: <2014-10-03 22:42:26 mtw>
+# Last changed Time-stamp: <2014-10-04 01:11:26 mtw>
 
 package Bio::ViennaNGS::AnnoC;
 
@@ -11,11 +11,12 @@ use warnings;
 use Data::Dumper;
 use Bio::Tools::GFF;
 use Bio::DB::Fasta;
+use IPC::Cmd qw(can_run run);
 use Path::Class;
 use Carp;
 
 our @ISA       = qw(Exporter);
-our @EXPORT_OK = qw(parse_gff feature_summary get_fasta_ids
+our @EXPORT_OK = qw(&parse_gff &feature_summary &get_fasta_ids &features2bed
 		    $feat $fstat $fastadb
 		    @fastaids
 		    %features %featsta);
@@ -124,6 +125,86 @@ sub parse_gff {
   #die;
 }
 
+# features2bed($featR,$fstatR,$feature,$dest,$bn,$log)
+# Convert genome annotation features to BED12
+# Returns a BED12 for each feature type in %features hash
+sub features2bed {
+ my ($featR,$fstatR,$feature,$dest,$bn,$log) = @_;
+ my ($chrom,$chrom_start,$chrom_end,$name,$score,$strand,$thick_start);
+ my ($thick_end,$reserved,$block_count,$block_sizes,$block_starts);
+ my @ft = ();
+ my $this_function = (caller(0))[3];
+ my $bedtools = can_run('bedtools') or
+   croak "ERROR [$this_function] Cannot find 'bedtools' utility";
+
+ croak "ERROR [$this_function] undef \%features hash"
+   unless (defined $featR);
+ croak "ERROT [$this_function] undef \%fstat hash"
+   unless (defined $fstatR);
+ croak "ERROR [$this_function] $dest does not exist"
+    unless (-d $dest);
+  if (defined $log){open(LOG, ">>", $log) or croak $!;}
+
+ if (defined $feature){ # dump one feature type
+   confess "ERROR [$this_function] feature type \'$feature\' N/A in hash "
+     unless (exists $$fstat{$feature});
+   $ft[0] = $feature;
+ }
+ else{ # dump all feature types
+   # get all feature types from in %$featR
+   foreach my $gbkey (keys %$fstatR) {
+     next if ($gbkey eq 'total' || $gbkey eq 'Src' ||
+	      $gbkey eq 'accession' || $gbkey eq 'origin');
+     push @ft,$gbkey;
+   }
+ }
+
+ foreach my $f (@ft){
+   my $bedname   = file($dest,"$bn.$f.bed");
+   my $bedname_u = file($dest,"$bn.$f.u.bed");
+   open (BEDOUT, "> $bedname_u") or croak $!;
+
+   # dump unsorted gene annotation from DS to BED12
+   foreach my $uid (keys %$featR){
+      next unless ($$featR{$uid}->{gbkey} eq $f);
+      my @bedline = ();
+      $chrom        = $$featR{$uid}->{seqid};
+      $chrom_start  = $$featR{$uid}->{start};
+      $chrom_start--; # BED is 0-based
+      $chrom_end    = $$featR{$uid}->{end};
+      $name         = $$featR{$uid}->{name};
+      $score        = $$featR{$uid}->{score};
+      $strand       = $$featR{$uid}->{strand} == -1 ? '-' : '+'; #default to +
+      $thick_start  = $chrom_start;
+      $thick_end    = $chrom_end;
+      $reserved     = 0; 
+      $block_count  = 1;
+      $block_sizes  = $$featR{$uid}->{length}.",";
+      $block_starts = "0,";
+      @bedline = join ("\t", ($chrom,$chrom_start,$chrom_end,
+			      $name,$score,$strand,$thick_start,
+			      $thick_end,$reserved,$block_count,
+			      $block_sizes, $block_starts));
+      print BEDOUT "@bedline\n";
+    }
+   close (BEDOUT);
+
+   # sort bed file
+   my $cmd = "$bedtools sort -i $bedname_u > $bedname";
+   my ( $success, $error_message, $full_buf, $stdout_buf, $stderr_buf ) =
+     run( command => $cmd, verbose => 0 );
+   if( !$success ) {
+     print STDERR "ERROR [$this_function] Call to $bedtools  unsuccessful\n";
+     print STDERR "ERROR: this is what the command printed:\n";
+     print join "", @$full_buf;
+     croak $!;
+   }
+   unlink($bedname_u);
+
+ } # end foreach
+ if (defined $log){close(LOG)};
+}
+
 # feature_summary($fsR,$dest)
 # Print summary of %featstat hash
 #
@@ -178,24 +259,29 @@ annotation formats
   use Bio::ViennaNGS::AnnoC;
 
   parse_gff($gff3_file);
+
   feature_summary($fstat,$dest);
+
+  features2bed($featR,$fstatR,$feature,$dest,$bn,$log)
+
   get_fasta_ids($fasta_file);
 
 =head1 DESCRIPTION
 
-=over 3
+=over 4
 
 =item parse_gff($gff3_file)
 
-C<parse_gff()> parses GFF3 annotation files. The GFF3 specification is
-available at L<http://www.sequenceontology.org/resources/gff3.html>
-This routine expects the path to a GFF3 file as argument C<$gff3_file>
-and returns two hash references: C<$feat> is a reference to a HOH
-containing the raw annotation information for each feature found in
-the GFF3 file. C<$fstat> references a hash containing summary statistics
-of the features found in the GFF3 file.
+C<parse_gff()> parses GFF3 annotation files of non-spliced
+genomes. The GFF3 specification is available at
+L<http://www.sequenceontology.org/resources/gff3.html> This routine
+expects the path to a GFF3 file as argument C<$gff3_file> and returns
+two hash references: C<$feat> is a reference to a HOH containing the
+raw annotation information for each feature found in the GFF3
+file. C<$fstat> references a hash containing summary statistics of the
+features found in the GFF3 file.
 
-This routine has been tested with NCBI bacteria GFF3 annotation. 
+This routine has been tested with NCBI bacteria GFF3 annotation.
 
 =item feature_summary($fstat,$dest)
 
@@ -203,6 +289,19 @@ This routine generates a summary file for all features parsed by
 parse_gff. It expects two arguments: C<$fstat> is a refence to the
 summary hash generated by C<parse_gff()> and C<$dest> is the output
 path for a summary.txt file.
+
+=item features2bed($featR,$fstatR,$feature,$dest,$bn,$log)
+
+Thsi routine converts genomic features from a L<Bio::ViennaNGS::AnnoC>
+C<%features> hash into BED12 format. C<$featR> and C<$fstatR> are
+references to the C<%features> and C<%featstat> hashes,
+respectively. C<$feature> can be either a string corresponding to a
+genbank key present in C<%features> or C<undef>. If it is defined,
+only features of the speficied key will be written to one single BED12
+file. If C<$feature> is undef, BED12 files will be generated for each
+genbank key present in C<%features>. C<$dest> is the output directory
+and C<$bn> the basename for all output files. C<$log> can either be
+the full path to a logfile or C<undef>.
 
 =item get_fasta_ids($fasta_file)
 
@@ -219,6 +318,8 @@ multiple entries.
 =item L<Bio::Tools::GFF>
 
 =item L<Bio::DB::Fasta>
+
+=item L<IPC::Cmd>
 
 =item L<Path::Class>
 
