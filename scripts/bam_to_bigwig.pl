4675b06bafe51f8e3795db0c024578e0db4578b4
Michael T. Wolfinger <michael@wolfinger.eu>
2014-12-03 14:17:24 +0100

    initial commit

---
+++ scripts/bam_to_bigwig.pl
@@ -0,0 +1,148 @@
+#!/usr/bin/env perl
+# -*-CPerl-*-
+# Last changed Time-stamp: <2014-12-03 14:11:20 mtw>
+#
+# ***********************************************************************
+# *  Copyright notice
+# *
+# *  Copyright 2014 Michael T. Wolfinger <michael@wolfinger.eu>
+# *  All rights reserved
+# *
+# *  This program is free software: you can redistribute it and/or modify
+# *  it under the terms of the GNU General Public License as published by
+# *  the Free Software Foundation, either version 3 of the License, or
+# *  (at your option) any later version.
+# *
+# *  This program is distributed in the hope that it will be useful,
+# *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+# *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# *  GNU General Public License for more details.
+# *
+# *  You should have received a copy of the GNU General Public License
+# *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# *
+# *  This copyright notice MUST APPEAR in all copies of the script!
+# ***********************************************************************
+
+use strict;
+use warnings;
+use Getopt::Long qw( :config posix_default bundling no_ignore_case );
+use Pod::Usage;
+use Data::Dumper;
+use IPC::Cmd qw(can_run);
+use Bio::ViennaNGS  qw(bed2bigBed);
+use Bio::ViennaNGS::SpliceJunc qw(bed6_ss_to_bed12);
+
+#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^#
+#^^^^^^^^^^ Variables ^^^^^^^^^^^#
+#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^#
+
+my ($bam_in);
+my $outdir = "./";
+my $want_bigbed = 0;
+my $cs_in = '-';
+my $strand = "+";
+my @result = ();
+
+#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^#
+#^^^^^^^^^^^^^^ Main ^^^^^^^^^^^^^#
+#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^#
+
+Getopt::Long::config('no_ignore_case');
+pod2usage(-verbose => 1) unless GetOptions("b|bam=s"    => \$bam_in,
+					   "c=s"        => \$cs_in,
+					   "o=s"        => \$outdir,
+					   "s|strand=s" => \$strand,
+					   "man"        => sub{pod2usage(-verbose => 2)},
+					   "help|h"     => sub{pod2usage(1)}
+					  );
+
+
+unless ($bam_in =~ /^\//) {$bam_in = "./".$bam_in;}
+unless (-f $bam_in){
+  warn "Could not find input file $bam_in given via --bam option";
+  pod2usage(-verbose => 0);
+}
+
+unless ($cs_in =~ /^\//) {$cs_in = "./".$cs_in;}
+unless (-f $cs_in){
+  warn "Could not find input file $cs_in given via -c option";
+  pod2usage(-verbose => 0);
+}
+
+unless ($strand =~ /[\+\-]/) { 
+  warn "Invalid value '$strand' given via -s option. Please specify
+  either '+' or '-' for positive or negative strand, respectively";
+  pod2usage(-verbose => 0);
+ }
+
+#TODO check if we are allowed to write to $outdir
+unless ($outdir =~ /\/$/){$outdir .= "/";}
+unless (-d $outdir){mkdir $outdir or die $!;}
+
+@result = bam_or_bed2bw("bam",$bam_in,$cs_in,$strand,$outdir,0,0,1.,undef);
+
+print Dumper(\@result);
+
+__END__
+
+
+=head1 NAME
+
+bam_to_bigWig.pl - Make bigWig coverage profiles from BAM files
+
+=head1 SYNOPSIS
+
+bam_to_bigWig.pl [--bam I<FILE>] [-c I<FILE>] [options]
+
+=head1 DESCRIPTION
+
+Produce bigWig coverage profiles from (aligned) BAM files, explicitly
+considering strandedness. The most natural use case of this tool is to
+create strand-aware coverage profiles in bigWig format for genome
+browser visualization.
+
+=head1 OPTIONS
+
+=over
+
+=item B<-b|--bam>
+
+Input file in BAM format
+
+=item B<-c>
+
+Chromosome sizes files
+
+=item B<-s|--strand>
+
+Use this option if the input BAM file is strictly strand-specific,
+ie. contains B<only> reads mapped to either the positive or negative
+strand. Possible values are either '+' or '-'. If the value given here
+is '+', the interim bedGraph file will be created with positive
+values. A '-' given here will create the inerim bedGraph file with
+negative values, which is required for proper visualization of bigWig
+files holding coverage profiles of reads mapped to the negative strand
+in the UCSC genome browser.
+
+=item B<-o>
+
+Relative output path
+
+
+=item B<--help -h>
+
+Print short help
+
+=item B<--man>
+
+Prints the manual page and exits
+
+=back
+
+=head1 AUTHOR
+
+Michael T. Wolfinger E<lt>michael@wolfinger.euE<gt>
+
+=cut
+
