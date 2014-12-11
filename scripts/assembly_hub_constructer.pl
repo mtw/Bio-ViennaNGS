7be80d1800e77f4cd149de2bf25d706ecdff5e99
Michael T. Wolfinger <michael@wolfinger.eu>
2014-12-11 17:24:30 +0100

    WiP

--- scripts/assembly_hub_constructer.pl
+++ scripts/assembly_hub_constructer.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/env perl
 # -*-CPerl-*-
-# Last changed Time-stamp: <2014-09-24 14:17:55 egg>
+# Last changed Time-stamp: <2014-12-11 15:42:36 mtw>
 #
 # Construct UCSC assembly hub
 # Display of novel genome sequences on the UCSC Genome Hub.
@@ -10,7 +10,7 @@
 # ***********************************************************************
 # *  Copyright notice
 # *
-# *  Copyright 2014 Michael Thomas Wolfinger <michael@wolfinger.eu>
+# *  Copyright 2014 Michael T. Wolfinger <michael@wolfinger.eu>
 # *                 Florian Eggenhofer <florian.eggenhofer@univie.ac.at>
 # *  All rights reserved
 # *
@@ -33,54 +33,118 @@
 use strict;
 use warnings;
 use Getopt::Long;
+use Pod::Usage;
 use Data::Dumper;
 use File::Basename;
+use Path::Class;
 use Bio::ViennaNGS::UCSC qw( make_assembly_hub );
 
 #^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^#
 #^^^^^^^^^^ Variables ^^^^^^^^^^^#
 #^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^#
-my ($fasta_file_path,$assembly_hub_destination_path,$base_URL,$assembly_hub_return_value,$log_path);
 
+my ($assembly_hub_return_value,$lf);
+my $logname = "assembly_hub_constructor.log";
+my $fasta_in = '-';
+my $folder_in = '-';
+my $dest = '-';
+my $base_URL = '-';
 #^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^#
 #^^^^^^^^^^^^^^ Main ^^^^^^^^^^^^^#
 #^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^#
 Getopt::Long::config('no_ignore_case');
-&usage() unless GetOptions("i=s"           => \$fasta_file_path,
-			   "a=s"           => \$assembly_hub_destination_path,
-                           "b=s"           => \$base_URL,
-                           "-help"         => \&usage,
-                           "v");
+pod2usage(-verbose => 1) unless GetOptions("fa|f=s"       => \$fasta_in,
+					   "infolder|i=s" => \$folder_in,
+					   "out|o=s"      => \$dest,
+					   "baseurl|b=s"  => \$base_URL,
+					   "man"          => sub{pod2usage(-verbose => 2)},
+					   "help|h"       => sub{pod2usage(1)}
+					  );
 
-die "ERROR in [assembly_hub_constructer.pl]: no input fasta file provided" unless(defined $fasta_file_path);
-die "ERROR in [assembly_hub_constructer.pl]: input fasta file not found" unless (-e $fasta_file_path);
-die "ERROR in [assembly_hub_constructer.pl]: no assembly hub destination path provided" unless(defined $assembly_hub_destination_path);
-die "ERROR in [assembly_hub_constructer.pl]: assembly hub destination path not writable" unless (-e $assembly_hub_destination_path);
-die "ERROR in [assembly_hub_constructer.pl]: no URL (network location for upload to UCSC) provided" unless(defined $base_URL);
-unless ($assembly_hub_destination_path =~ /\/$/){$assembly_hub_destination_path.= "/";}
-$log_path = $assembly_hub_destination_path . "Log/";
-unless (-d $log_path){my $cmd = "mkdir -p  $log_path"; system($cmd);}
-
-$assembly_hub_return_value = make_assembly_hub($fasta_file_path,$assembly_hub_destination_path,$base_URL,$log_path);
-
-#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^#
-#^^^^^^^^^^^ Subroutines ^^^^^^^^^^#
-#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^#
-
-
-sub usage {
-  print <<EOF;
-Build assembly hubs for the UCSC genome browser. The input is fasta file, optionally big bed files for tracks
-(placed in the same folder), a output path for the resulting assembly hub folder and the URL where this
-folder will be made available for upload to UCSC.
-
-usage: $0 [options]
-program specific options:                                default:
- -i      <file>                                          ($fasta_file_path)
- -a      <path>   output directory                       ($assembly_hub_destination_path)
- -b      <url>    URL - network location for upload to UCSC                            ($base_URL)
- -help            print this information
-
-EOF
-exit;
+unless (-e $fasta_in){
+  warn "Could not find input Fasta file given via --fa option";
+  pod2usage(-verbose => 0);
 }
+unless (-d $folder_in){
+  warn "An input folder path is required and must be gi ven via --infolder option";
+  pod2usage(-verbose => 0);
+}
+unless ($base_URL =~ /^http/) {
+  warn "Base URL must be given in full format, eg http://foo.bar.com/Hubs/";
+  pod2usage(-verbose => 0);
+}
+unless ($dest =~ /\/$/){$dest.= "/";}
+unless (-d $dest){
+  mkdir $dest or die $!;
+}
+$lf = file($dest,$logname);
+
+$assembly_hub_return_value = make_assembly_hub($fasta_in,$dest,$base_URL,$lf);
+
+
+__END__
+
+
+=head1 NAME
+
+assembly_hub_constructor.pl - Build UCSC genome browser Assembly Hubs
+from genomic sequence and annotation
+
+=head1 SYNOPSIS
+
+assembly_hub_constructor.pl [--fa I<FILE>] [--infolder I<PATH>] [--out
+I<PATH>] [--baseurl -I<URL>] [options]
+
+=head1 DESCRIPTION
+
+The UCSC genome browser offers the possibility to visualize any
+organism (including organisms that are not included in the standard
+UCSC browser bundle) through hso called 'Assembly Hubs'. This script
+constructs Assembly Hubs from genomic sequence and annotation data.
+
+=head1 OPTIONS
+
+=over
+
+=item B<--bam>
+
+=item B<--fa -f>
+
+Input file in Fasta format.
+
+=item B<--infolder -i>
+
+Directory which contains all track files in BED/bigBed format. The
+resulting Assembly Hub will contain these files in their respective
+bigFile version.
+
+=item B<--out -o>
+
+Destination folder for the output Assembly Hub.
+
+=item  B<--baseurl -b>
+
+BaseURL used within the Assembly Hub. This URL will be included
+verbatim in the resulting Assembly Hub. It is crucial that this URl is
+valid, else the resulting Assembly Hub will be broken.
+
+=item B<--help -h>
+
+Print short help.
+
+=item B<--man>
+
+Prints the manual page and exits.
+
+=back
+
+=head1 SEE ALSO
+
+The L<UCSC Genome
+Wiki|http://genomewiki.ucsc.edu/index.php/Assembly_Hubs> has extensive
+documentation for Assembly Hubs.
+
+=head1 AUTHORS
+
+Florian Eggenhofer E<lt>florian.eggenhofer@univie.ac.atE<gt>
+Michael T. Wolfinger E<lt>michael@wolfinger.euE<gt>
