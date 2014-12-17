2461c8f6a072f9260e6d80d35bc220821ee731a7
Michael T. Wolfinger <michael@wolfinger.eu>
2014-12-17 18:35:47 +0100

    modified:   lib/Bio/ViennaNGS/BamStatSingle.pm 	modified:   scripts/bam_stat_test.pl

--- lib/Bio/ViennaNGS/BamStatSingle.pm
+++ lib/Bio/ViennaNGS/BamStatSingle.pm
@@ -1,5 +1,5 @@
 # -*-CPerl-*-
-# Last changed Time-stamp: <2014-12-17 00:11:48 mtw>
+# Last changed Time-stamp: <2014-12-17 18:24:36 mtw>
 
 package Bio::ViennaNGS::BamStatSingle;
 
@@ -8,6 +8,8 @@ use version; our $VERSION = qv('0.12_05');
 use Bio::DB::Sam 1.39;
 use Moose;
 use Carp;
+use POSIX qw(floor);
+use Statistics::R;
 use Data::Dumper;
 use namespace::autoclean;
 
@@ -16,25 +18,419 @@ has 'bam' => (
 	      isa => 'Str',
 	      required => 1,
 	      predicate => 'has_bam',
-	      trigger => \&_parse_bam,
 	     );
 
 has 'data' => (
 	       is => 'ro',
 	       isa => 'HashRef',
-	       predicate => 'has_data',	       
+	       predicate => 'has_data',
 	      );
 
-sub _parse_bam {
+has 'control_match' => ( # provides stats how many mapped bases match the reference genome
+			is => 'rw',
+			isa => 'Bool',
+			default => '1',
+			clearer => 'clear_control_match',
+			predicate => 'has_control_match',
+		       );
+
+has 'control_clip' => ( # provides stats how many bases are soft or hard clipped
+		       is => 'rw',
+		       isa => 'Bool',
+		       default => '1',
+		       clearer => 'clear_control_clip',
+		       predicate => 'has_control_clip',
+		       );
+
+has 'control_split' => ( # provides stats how many/often  mapped reads are split
+			is => 'rw',
+			isa => 'Bool',
+			default => '1',
+			clearer => 'clear_control_split',
+			predicate => 'has_control_split',
+		       );
+
+has 'control_qual' => ( # provides stats on quality of the match
+		       is => 'rw',
+		       isa => 'Bool',
+		       default => '1',
+		       clearer => 'clear_control_qual',
+		       predicate => 'has_control_qual',
+		      );
+
+has 'control_edit' => ( # provides stats on the edit distance between read and mapped reference
+		       is => 'rw',
+		       isa => 'Bool',
+		       default => '1',
+		        clearer => 'clear_control_edit',
+		       predicate => 'has_control_edit',
+		      );
+
+has 'control_flag' => ( # analyses the sam bit flag for qual/strands/pair_vs_single reads
+		       is => 'rw',
+		       isa => 'Bool',
+		       default => '1',
+		       clearer => 'clear_control_flag',
+		       predicate => 'has_control_flag',
+		      );
+
+has 'control_score' => ( # provides stats on per-base quality scores
+			is => 'rw',
+			isa => 'Bool',
+			default => '1',
+			clearer => 'clear_control_score',
+			predicate => 'has_control_score',
+		       );
+
+has 'control_uniq' => ( #  gives number and stats of multiplicity of readaligments
+		       is => 'rw',
+		       isa => 'Bool',
+		       default => '1',
+		       clearer => 'clear_control_uniq',
+		       predicate => 'has_control_uniq',
+		      );
+
+has 'is_segemehl' => ( # toggles to consider segemehl specific bam feature
+		      is => 'rw',
+		      isa => 'Bool',
+		      default => '0',
+		      predicate => 'has_is_segemehl',
+		     );
+
+has 'data_out' => (
+		     is => 'rw',
+		     isa => 'HashRef',
+		     default => sub { {} },
+		     predicate => 'has_data_out',
+		    );
+
+has 'data_match' => (
+		     is => 'rw',
+		     isa => 'ArrayRef',
+		     default => sub { [] },
+		     predicate => 'has_data_match',
+		    );
+
+has 'data_qual' => (
+		    is => 'rw',
+		    isa => 'ArrayRef',
+		    default => sub { [] },
+		    predicate => 'has_data_qual',
+		   );
+
+has 'data_edit' => (
+		    is => 'rw',
+		    isa => 'ArrayRef',
+		    default => sub { [] },
+		    predicate => 'has_data_edit',
+		   );
+
+has 'data_score' => (
+		    is => 'rw',
+		    isa => 'ArrayRef',
+		    default => sub { [] },
+		    predicate => 'has_data_score',
+		   );
+
+has 'data_clip' => (
+		    is => 'rw',
+		    isa => 'ArrayRef',
+		    default => sub { [] },
+		    predicate => 'has_data_clip',
+		   );
+
+has 'data_split' => (
+		     is => 'rw',
+		     isa => 'HashRef',
+		     default => sub { {} },
+		     predicate => 'has_data_split',
+		    );
+
+has 'data_flag' => (
+		    is => 'rw',
+		    isa => 'HashRef',
+		    default => sub { {} },
+		    predicate => 'has_data_flag',
+		   );
+
+has 'data_uniq' => (
+		    is => 'rw',
+		    isa => 'HashRef',
+		    default => sub { {} },
+		    predicate => 'has_data_uniq',
+		   );
+
+sub stat_singleBam {
   my ($self) = @_;
   my $this_function = (caller(0))[3];
   confess "ERROR [$this_function] Attribute 'bam' not found $!"
     unless ($self->has_bam);
-  print "parsing BAM file $self->bam ...\n";
+
+  ## read in bam file using Bio::DB::Sam
+  #my $sam = Bio::DB::Sam->new(-bam   => $self->bam);
+  my $bam = Bio::DB::Bam->open($self->bam);
+
+  my $header       = $bam->header;
+  my $target_count = $header->n_targets;
+  my @chromosomes = @{$header->target_name};
+  my $target_names = \@chromosomes;
+
+  while (my $align = $bam->read1) {
+    my %flags = ();
+    my $qname      = $align->qname;
+    my $seqid      = $target_names->[$align->tid];
+    my $start      = $align->pos+1;
+    my $end        = $align->calend;
+    my $strand     = $align->strand;
+    my $cigar      = $align->cigar_str;
+    my @scores     = $align->qscore;
+    my $match_qual = $align->qual;
+    my $flag       = $align->flag;
+    my $attributes = $align->aux;
+    #    print "\$seqid: $seqid\t \$qname: $qname\t $start: $start\t \$end: $end\t \$cigar: $cigar\t\$strand: $strand\t \$match_qual: $match_qual\t \$attributes: $attributes\n";
+    my @tags = $align->get_all_tags;
+    #    print ">> tags: @tags\n";
+
+    #############################
+    ### flag
+    if ($self->has_control_flag){
+
+      my $multisplit_weight = 1;
+      if ($self->has_is_segemehl && $align->has_tag('XL') ) {
+	$multisplit_weight = (1/($align->aux_get('XL')));
+      }
+
+      if( $align->get_tag_values('PAIRED')) {
+	${$self->data_flag}{'paired'}->{'paired-end'}+=$multisplit_weight;
+      }else{
+	${$self->data_flag}{'paired'}->{'single-end'}+=$multisplit_weight;
+      }
+
+      if( $align->get_tag_values('REVERSED')) {
+	${$self->data_flag}{'strand'}->{'reverse'}+=$multisplit_weight;
+      }else{
+	${$self->data_flag}{'strand'}->{'forward'}+=$multisplit_weight;
+      }
+
+      if( $align->get_tag_values('PAIRED') && $align->get_tag_values('M_UNMAPPED')){
+	${$self->data_flag}{'pairs'}->{'unmapped_pair'}+=$multisplit_weight;
+      }
+      elsif( $align->get_tag_values('PAIRED') && !$align->get_tag_values('M_UNMAPPED')) {
+	${$self->data_flag}{'pairs'}->{'mapped_pair'}+=$multisplit_weight;
+      }
+
+      if( $align->get_tag_values('DUPLICATE') ){
+	${$self->data_flag}{'unmapped'}->{'duplicated'}+=$multisplit_weight;
+      }
+      if( $align->get_tag_values('QC_FAILED') ){
+	${$self->data_flag}{'unmapped'}->{'qual_failed'}+=$multisplit_weight;
+      }
+      if( $align->get_tag_values('UNMAPPED') ){
+	${$self->data_flag}{'unmapped'}->{'unmapped'}+=$multisplit_weight;
+      }
+    }
+
+    #############################
+    ### uniq
+    if ($self->has_control_uniq){
+
+      my $multisplit_weight = 1;
+      if ($self->has_is_segemehl && $align->has_tag('XL') ) {
+	$multisplit_weight = ($align->aux_get('XL'));
+      }
+
+      my $multimap_weight = 1;
+      if ( $align->has_tag('NH') ) {
+	$multimap_weight = ($align->aux_get('NH'));
+      }
+
+      ${$self->data_uniq}{'uniq'}+=(1/$multisplit_weight) if($multimap_weight == 1);
+      ${$self->data_uniq}{'mapped'}+=1/($multimap_weight*$multisplit_weight);
+      ${$self->data_uniq}{'multiplicity'}->{$multimap_weight}+=(1/$multisplit_weight);
+
+    }
+
+    #############################
+    ### Quality Score read
+    if($self->has_control_qual){
+      if($self->has_is_segemehl && $match_qual == 255){
+	carp "WARN [$this_function] no match_qual for segemehl";
+	$self->clear_control_qual;
+      }
+      elsif($match_qual){
+	push @{$self->data_qual}, $match_qual;
+      }
+      else{
+	carp "WARN [$this_function] No matchqual for read $qname";
+	carp "WARN [$this_function] Setting \$self->has_control_qual to zero";
+	$self->clear_control_qual;
+      }
+    }
+
+    #############################
+    #### Edit distance
+    if($self->has_control_edit){
+      if( $align->has_tag('NM') ){
+	push @{$self->data_edit}, $align->aux_get('NM');
+      }
+      else{
+	carp "WARN [$this_function] No <NM> attribute for read $qname";
+	carp "WARN [$this_function] Setting \$self->has_control_edit to zero";
+	$self->clear_control_edit;
+      }
+    }
+
+    #############################
+    ### Match bases
+    if($self->has_control_match){
+      if($cigar){
+	push @{$self->data_match},
+	  sprintf("%.2f", 100*(&cigarmatch($cigar))/(&cigarlength($cigar)));
+      }
+      else{
+	carp "WARN [$this_function] No CIGAR string for read $qname";
+	carp "WARN [$this_function] Setting \$self->has_control_match to zero";
+	$self->clear_control_match;
+      }
+    }
+
+    #############################
+    ### Clip reads
+    if($self->control_clip){
+      if($cigar){
+	push @{$self->data_clip}, sprintf("%.2f", 100*(&cigarclip($cigar)));
+      }
+      else{
+	carp "WARN [$this_function] No CIGAR string for read $qname";
+	carp "WARN [$this_function] Setting \$self->has_control_clip to zero";
+	$self->clear_control_clip;
+        }
+      }
+
+    #############################
+    ### Split reads
+    if($self->control_split){
+      if ($self->has_is_segemehl && $align->has_tag('XL') ) {
+	my $split_counts = $align->aux_get('XL');
+	${$self->data_split}{$split_counts}+=1/($split_counts);
+	${$self->data_split}{'total'}+=1/($split_counts);
+      }
+      elsif ( $self->has_is_segemehl ){ }
+      else{
+	if($cigar){
+	  my $split_counts = cigarsplit($cigar);
+	  ${$self->data_split}{$split_counts}++;
+	  ${$self->split_data}{'total'}++;
+	}
+	else{
+	  carp "WARN [$this_function] No CIGAR string for read $qname";
+	  carp "WARN [$this_function] Setting \$self->has_control_split to zero";
+	  $self->clear_control_split;
+	}
+      }
+    }
+
+  } # end while
+
+
+  #############################
+  ### Extract reference genome/chromosome sizes from BAM header
+  for (my $i=0;$i<$header->n_targets;$i++){
+    my $seqid = $header->target_name->[$i];
+    my $length = $header->target_len->[$i];
+    ${$self->data_out}{'chrom'}->{$seqid}=$length;
+ }
+
+  #############################
+  #### uniq
+  if ($self->has_control_uniq){
+    ${$self->data_out}{'uniq'}->{'uniq_mapped_reads'}=${$self->data_uniq}{'uniq'};
+    ${$self->data_out}{'uniq'}->{'mapped_reads'}=sprintf("%d", ${$self->data_uniq}{'mapped'});
+    ${$self->data_out}{'uniq'}->{'uniq_mapped_reads_percent'}=
+      sprintf("%.2f", (100*${$self->data_uniq}{'uniq'}/${$self->data_uniq}{'mapped'}));
+    foreach my $multiplicity (sort {$a cmp $b} keys %{${$self->data_uniq}{'multiplicity'}}) {
+      ${$self->data_out}{'uniq'}->{'distribution_of_multimapper'}->{"<$multiplicity>"}=
+	(${$self->data_uniq}{'multiplicity'}->{$multiplicity}/$multiplicity);
+    }
+  }
 
 }
 
 __PACKAGE__->meta->make_immutable;
+
+
+no Moose;
+
+sub stats{
+  # usage: %h = %{stats(@a)};
+  my @vals = sort {$a <=> $b} @_;
+  my %stats = ();
+  my $median = '';
+  
+  if(@vals%2){$stats{'med'} = $vals[int(@vals/2)];}                      #odd median
+  else{$stats{'med'} = ($vals[int(@vals/2)-1] + $vals[int(@vals/2)])/2;} #even median
+  $stats{'med'}             = sprintf("%.2f", $stats{'med'});
+  $stats{'mean'}            = sprintf("%.2f", &mean(\@vals));            ## mean
+  $stats{'min'}             = sprintf("%.2f", &min(\@vals));             ## min
+  $stats{'max'}             = sprintf("%.2f", &max(\@vals));             ## max
+  $stats{'1q'}              = sprintf("%.2f", $vals[int(@vals/4)]);      ## 1.quartile
+  $stats{'3q'}              = sprintf("%.2f", $vals[int((@vals*3)/4)]);  ## 3.quartile
+
+  return(\%stats);
+}
+
+sub mean { # usage: $h = %{mean(\@a)};
+  my ($arrayref) = @_;
+  my $sum;
+  foreach (@$arrayref) {$sum += $_}
+  return $sum / @$arrayref;
+}
+
+sub max { # usage: $h = %{max(\@a)};
+  my ($arrayref) = @_;
+  my $max = $arrayref->[0];
+  foreach (@$arrayref) {$max = $_ if $_ > $max}
+  return $max;
+}
+
+sub min { # usage: $h = %{min(\@a)};
+  my ($arrayref) = @_;
+  my $min = $arrayref->[0];
+  foreach (@$arrayref) {$min = $_ if $_ < $min}
+  return $min;
+}
+
+sub cigarlength { #usage: &cigarlength($cigarstring)
+  my $cigar_string = shift;
+  my $cigar_length = 0;
+  while($cigar_string=~m/(\d+)[MIX=]/g){$cigar_length+=$1}
+  return($cigar_length);
+}
+
+sub cigarmatch { #usage: cigarmatch($cigarstring)
+  my $cigar_string = shift;
+  my $cigar_match  = 0;
+  while($cigar_string=~m/(\d+)[M=]/g){$cigar_match+=$1}
+  return($cigar_match);
+}
+
+sub cigarsplit { #usage: cigarsplit($cigarstring)
+  my $cigar_string = shift;
+  my $cigar_split  = 0;
+  while($cigar_string=~m/N/g){$cigar_split+=1}
+  return($cigar_split);
+}
+
+sub cigarclip { #usage: cigarclip($cigarstring)
+  my $cigar_string = shift;
+  my $cigar_length = 0;
+  my $cigar_clip   = 0;
+  while($cigar_string=~m/(\d+)[SH]/g) {$cigar_clip+=$1}
+  while($cigar_string=~m/(\d+)\D/g){$cigar_length+=$1}
+  return ($cigar_length==0)?(0):($cigar_clip/$cigar_length);
+}
+
+
 
 1;
 __END__
--- scripts/bam_stat_test.pl
+++ scripts/bam_stat_test.pl
@@ -1,11 +1,19 @@
 #!/usr/bin/env perl
 # -*-CPerl-*-
-# Last changed Time-stamp: <2014-12-17 00:29:23 mtw>
+# Last changed Time-stamp: <2014-12-17 17:52:17 mtw>
 
 use Bio::ViennaNGS::BamStatSingle;
 use Data::Dumper;
+use File::Share ':all';
 
-my $bamfile = "/path/to/file.bam";
+
+#my $bamfile = "/home/mescalin/mtw/Perl/MyModules/Bio-ViennaNGS/share/data1/SRR794848.8k.bam";
+my $bamfile = "/scratch/mtw/tmp/J00027/splits.bam/E1_R1.uniq.splits.bam";
+#my $bamfile = "/scratch/mtw/tmp/encode.bam";
+my $bamfile = "/scratch/mtw/tmp/encode30.bam";
+
+#my $bamfile = dist_file('Bio-ViennaNGS','SRR794848.8k.bam');
+
 my %hash = ( 'foo' => { 'aaa' => 'franz',
 			'bbb' => 'fritz'},
 	     'bar' => { 'aaa' => 'mitzi',
@@ -14,8 +22,10 @@ my %hash = ( 'foo' => { 'aaa' => 'franz',
 
 my $bss1 = Bio::ViennaNGS::BamStatSingle->new(
 					      bam => $bamfile,
-					      data => \%hash,
+					      control_clip => 1,
 					     );
+
+$bss1->stat_singleBam();
 
 
 
