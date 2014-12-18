94a2c5bb3cdcb56c549bb918dc3003bb5fbe98ad
Michael T. Wolfinger <michael@wolfinger.eu>
2014-12-18 14:19:09 +0100

    moved BamStatSingle.pm -> BamStat.pm

--- scripts/bam_stat_test.pl
+++ scripts/bam_stat_test.pl
@@ -1,24 +1,21 @@
 #!/usr/bin/env perl
 # -*-CPerl-*-
-# Last changed Time-stamp: <2014-12-17 17:52:17 mtw>
+# Last changed Time-stamp: <2014-12-18 14:18:02 mtw>
 
-use Bio::ViennaNGS::BamStatSingle;
+use Bio::ViennaNGS::BamStat;
 use Data::Dumper;
 use File::Share ':all';
 
+my @files = ();
 
 #my $bamfile = "/home/mescalin/mtw/Perl/MyModules/Bio-ViennaNGS/share/data1/SRR794848.8k.bam";
 my $bamfile = "/scratch/mtw/tmp/J00027/splits.bam/E1_R1.uniq.splits.bam";
-#my $bamfile = "/scratch/mtw/tmp/encode.bam";
-my $bamfile = "/scratch/mtw/tmp/encode30.bam";
+my $bamfile = "/scratch/mtw/tmp/encode.bam";
+#my $bamfile = "/scratch/mtw/tmp/encode30.bam";
 
 #my $bamfile = dist_file('Bio-ViennaNGS','SRR794848.8k.bam');
 
-my %hash = ( 'foo' => { 'aaa' => 'franz',
-			'bbb' => 'fritz'},
-	     'bar' => { 'aaa' => 'mitzi',
-			'bbb' => 'zenzi'}
-	   );
+push (@files, $bamfile);
 
 my $bss1 = Bio::ViennaNGS::BamStatSingle->new(
 					      bam => $bamfile,
@@ -30,3 +27,4 @@ $bss1->stat_singleBam();
 
 
 print Dumper($bss1);
+print Dumper(\@files);
