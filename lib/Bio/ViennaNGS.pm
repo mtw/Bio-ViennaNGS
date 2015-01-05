a51ee2fcc4008b3f3f417cac6ce1070f801d0543
Michael T. Wolfinger <michael@wolfinger.eu>
2015-01-06 00:20:23 +0100

    modified:   README 	modified:   lib/Bio/ViennaNGS.pm

--- lib/Bio/ViennaNGS.pm
+++ lib/Bio/ViennaNGS.pm
@@ -1,9 +1,9 @@
 # -*-CPerl-*-
-# Last changed Time-stamp: <2014-12-20 00:30:22 mtw>
+# Last changed Time-stamp: <2015-01-06 00:18:32 mtw>
 
 package Bio::ViennaNGS;
 
-use version; our $VERSION = qv('0.12_07');
+use version; our $VERSION = qv('0.12_08');
 
 1;
 
@@ -58,99 +58,126 @@ for atomic tasks in NGS data processing:
 
 =over
 
-assembly_hub_constructor.pl:
-The UCSC genome browser offers the possibility to visualize any
-organism (including organisms that are not included in the standard
-UCSC browser bundle) through hso called 'Assembly Hubs'. This script
-constructs Assembly Hubs from genomic sequence and annotation data.
+=item F<assembly_hub_constructor.pl>: The UCSC genome browser offers
+the possibility to visualize any organism (including organisms that
+are not included in the standard UCSC browser bundle) through hso
+called 'Assembly Hubs'. This script constructs Assembly Hubs from
+genomic sequence and annotation data.
 
-bam_split.pl: 
-Split (paired-end and single-end) BAM alignment files by strand and compute
-statistics. Optionally create BED output, as well as normalized bedGraph
-and bigWig files for coverage visualization in genome browsers (see
-dependencies on third-patry tools below).
+=item F<bam_split.pl>: Split (paired-end and single-end) BAM alignment
+files by strand and compute statistics. Optionally create BED output,
+as well as normalized bedGraph and bigWig files for coverage
+visualization in genome browsers (see dependencies on third-patry
+tools below).
 
-bam_to_bigWig.pl:
-Produce bigWig coverage profiles from (aligned) BAM files, explicitly
-considering strandedness. The most natural use case of this tool is to
-create strand-aware coverage profiles in bigWig format for genome browser
-visualization.
+=item F<bam_to_bigWig.pl>: Produce bigWig coverage profiles from
+(aligned) BAM files, explicitly considering strandedness. The most
+natural use case of this tool is to create strand-aware coverage
+profiles in bigWig format for genome browser visualization.
 
-bam_uniq.pl:
-Extract unique and multi mapping reads from BAM alignment files and create
-a separate BAM file for both uniqe (.uniq.) and multi (.mult.) mappers.
+=item F<bam_uniq.pl>: Extract unique and multi mapping reads from BAM
+alignment files and create a separate BAM file for both uniqe (.uniq.)
+and multi (.mult.) mappers.
 
-bed2bedGraph.pl:
-Convert BED files to (strand specific) bedGraph files, allowing
-additional annotation and automatic generation of bedGraph files which can
-easily be converted to big-type files for easy UCSC visualization.
+=item F<bed2bedGraph.pl>: Convert BED files to (strand specific)
+bedGraph files, allowing additional annotation and automatic
+generation of bedGraph files which can easily be converted to big-type
+files for easy UCSC visualization.
 
-extend_bed.pl:
-Extend genomic features in BED files by a certain number of nucleotides,
-either on both sides or specifically at the 5' or 3' end, respectively.
+=item F<extend_bed.pl>: Extend genomic features in BED files by a
+certain number of nucleotides, either on both sides or specifically at
+the 5' or 3' end, respectively.
 
-gff2bed.pl:
-Convert RefSeq GFF3 annotation files to BED12 format. Individual BED12
-files are created for each feature type (CDS/tRNA/rRNA/etc.). Tested with
-RefSeq bacterial GFF3 annotation.  
+=item F<gff2bed.pl>: Convert RefSeq GFF3 annotation files to BED12
+format. Individual BED12 files are created for each feature type
+(CDS/tRNA/rRNA/etc.). Tested with RefSeq bacterial GFF3 annotation.
 
-kmer_analysis.pl:
-Count k-mers of predefined length in FastQ and Fasta files
+=item F<kmer_analysis.pl>: Count k-mers of predefined length in FastQ
+and Fasta files
 
-MEME_XML_motif_extractor.pl:
-Compute simple statistics from MEME XML output and return a list of found
-motifs with the number of sequences containing those motifs as well as nice
-ggplot graphs.
+=item F<MEME_XML_motif_extractor.pl>: Compute simple statistics from
+MEME XML output and return a list of found motifs with the number of
+sequences containing those motifs as well as nice ggplot graphs.
 
-motiffinda.pl: 
-Find motifs in annotated sequence features. The motif can be provided as
-regular expression.
+=item F<motiffinda.pl>: Find motifs in annotated sequence
+features. The motif can be provided as regular expression.
 
-newUCSCdb.pl:
-Create a new genome database (ie. add a novel organism) in a local
-instance of the UCSC genome browser.
+=item F<newUCSCdb.pl>: Create a new genome database (ie. add a novel
+organism) in a local instance of the UCSC genome browser.
 
-normalize_multicov.pl:
-Compute normalized expression data in TPM/RPKM from (raw) read counts in
-bedtools multicov format. TPM reference: Wagner et al, Theory
-Biosci. 131(4), pp 281-85 (2012)
+=item F<normalize_multicov.pl>: Compute normalized expression data in
+TPM/RPKM from (raw) read counts in bedtools multicov format. TPM
+reference: Wagner et al, Theory Biosci. 131(4), pp 281-85 (2012)
 
-sj_visualizer.pl:
-Convert splice junctions from mapped RNA-seq data in segemehl BED6 splice
-junction format to BED12 for easy visualization in genome Browsers.
+=item F<sj_visualizer.pl>: Convert splice junctions from mapped
+RNA-seq data in segemehl BED6 splice junction format to BED12 for easy
+visualization in genome Browsers.
 
-splice_site_summary.pl:
-Identify and characterize splice junctions from RNA-seq data by
-intersecting them with annotated splice junctions.
+=item F<splice_site_summary.pl>: Identify and characterize splice
+junctions from RNA-seq data by intersecting them with annotated splice
+junctions.
 
-newUCSCdb.pl:
-Create a new genome database for a locally installed instance of the UCSC
-genome browser. Based on
+=item F<newUCSCdb.pl>: Create a new genome database for a locally
+installed instance of the UCSC genome browser. Based on
 http://genomewiki.ucsc.edu/index.php/Building_a_new_genome_database
 
-trim_fastq.pl:
-Trim sequence and quality string fields in a Fastq file
-by user defined length.
+=item F<trim_fastq.pl>: Trim sequence and quality string fields in a
+Fastq file by user defined length.
 
 =back
 
 =head1 DEPENDENCIES
 
-=over 7
+The L<Bio::ViennaNGS> modules and classes depend on a set of Perl
+modules, some of which are part of the Perl core distribution:
 
-=item  L<Bio::Perl> >= 1.00690001
+=over 16
 
-=item  L<BIO::DB::Sam> >= 1.39
+=item L<Bio::Perl> >= 1.00690001
 
-=item  L<File::Basename>
+=item L<Bio::DB::Sam> >= 1.39
 
-=item  L<File::Temp>
+=item L<Bio::DB::Fasta>
 
-=item  L<Path::Class>
+=item L<Bio::Tools::GFF>
 
-=item  L<IPC::Cmd>
+=item L<File::Basename>
 
-=item  L<Carp>
+=item L<File::Share>
+
+=item L<File::Temp>
+
+=item L<Path::Class>
+
+=item L<IPC::Cmd>
+
+=item L<Carp>
+
+=item L<Template>
+
+=item L<Moose>
+
+=item L<Moose::Util::TypeConstraints>
+
+=item L<namespace::autoclean>
+
+=item L<MooseX::Clone>
+
+=item L<Tie::Hash::Indexed>
+
+=back
+
+In addition the following modules are required by the L<Bio::ViennaNGS> utilities:
+
+=over 4
+
+=item L<PerlIO::gzip>
+
+=item L<Math::Round>
+
+=item L<XML::Simple>
+
+=item L<Statistics::R>
 
 =back
 
@@ -163,34 +190,62 @@ available on your system, and that hey can be found and executed by
 the Perl interpreter. We recommend installing the latest version of
 L<BEDtools|https://github.com/arq5x/bedtools2> on your system.
 
+=head1 SOURCE AVAILABILITY
+
+Source code for this distribution is available from the L<ViennaNGS
+Github repository|https://github.com/mtw/Bio-ViennaNGS>.
+
+=head1 PAPERS
+
+If the L<Bio::ViennaNGS> suite is useful for your work or if you use
+any component of the distribution in a custom pipeline, please cite
+the following publication:
+
+B<"ViennaNGS - A toolbox for building efficient next-generation sequencing
+analysis pipelines">
+
+I<Michael T. Wolfinger, Joerg Fallmann, Florian Eggenhofer and Fabian Amman>
+
+bioRxiv L<doi:10.1101E<sol>013011|http://dx.doi.org/10.1101/013011>.
+
 =head1 NOTES
 
 The L<Bio::ViennaNGS> suite is actively developed and tested on
 different flavours of Linux and Mac OS X. We have taken care of
 writing platform-independent code that should run out of the box on
 most UNIX-based systems, however we do not have access to machines
-running Microsoft Windows. To put it straight: We have not tested and
-will not test Windows compatibility. 
+running Microsoft Windows. As such, we have not tested and will not
+test Windows compatibility.
 
 =head1 SEE ALSO
 
-=over 8
-
-=item L<Bio::ViennaNGS::Util>
+=over 13
 
 =item L<Bio::ViennaNGS::AnnoC>
 
-=item L<Bio::ViennaNGS::UCSC>
+=item L<Bio::ViennaNGS::Bam>
 
-=item L<Bio::ViennaNGS::SpliceJunc>
+=item L<Bio::ViennaNGS::BamStat>
+
+=item L<Bio::ViennaNGS::BamStatSummary>
 
 =item L<Bio::ViennaNGS::Fasta>
-
-=item L<Bio::ViennaNGS::MinimalFeature>
 
 =item L<Bio::ViennaNGS::Feature>
 
 =item L<Bio::ViennaNGS::FeatureChain>
+
+=item L<Bio::ViennaNGS::MinimalFeature>
+
+=item L<Bio::ViennaNGS::SpliceJunc>
+
+=item L<Bio::ViennaNGS::Util>
+
+=item L<Bio::ViennaNGS::Tutorial>
+
+=item L<Bio::ViennaNGS::UCSC>
+
+=item L<Bio::ViennaNGS::Util>
 
 =back
 
@@ -205,7 +260,6 @@ will not test Windows compatibility.
 =item Florian Eggenhofer E<lt>florian.eggenhofer@tbi.univie.ac.atE<gt>
 
 =item Fabian Amman E<lt>fabian@tbi.univie.ac.at<gt>
-
 
 =back
 
