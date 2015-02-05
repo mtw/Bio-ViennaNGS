cff9eb803f40bef0033fbfb6188db8ff6cd49ab4
Michael T. Wolfinger <michael@wolfinger.eu>
2015-02-05 16:35:38 +0100

    import of Bio::ViennaNGS::FeatureLine

---
+++ lib/Bio/ViennaNGS/FeatureLine.pm
@@ -0,0 +1,43 @@
+# -*-CPerl-*-
+# Last changed Time-stamp: <2015-02-05 16:33:40 mtw>
+
+package Bio::ViennaNGS::FeatureLine;
+
+use version; our $VERSION = qv('0.12_14');
+use namespace::autoclean;
+use Moose;
+extends 'Bio::ViennaNGS::MinimalFeature';
+
+has 'id' => (
+	     is => 'rw',
+	     isa => 'Str', # e.g. a transcript ID
+	     required => '1',
+	    );
+
+has 'fc' => (
+	     is => 'rw',
+	     traits => ['Hash'],
+	     isa => 'HashRef',
+	     required => '1',
+	     builder => '_build_fc',
+	     auto_deref => '1',
+	     handles => {
+			 add => 'set',
+			 elements => 'elements',
+			 kv => 'kv',
+			 count => 'count',
+			 lookup => 'accessor',
+			},
+	    );
+
+
+
+sub _build_fc {
+  tie my %hash, 'Tie::Hash::Indexed';
+  return \%hash; 
+}
+
+no Moose;
+
+1;
+
