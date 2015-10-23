6a8eb52ca5b5c2ca3ab48bfb0af8903f02e28891
Michael T. Wolfinger <michael@wolfinger.eu>
2015-10-23 16:44:31 +0200

    added POD

--- lib/Bio/ViennaNGS/Feature.pm
+++ lib/Bio/ViennaNGS/Feature.pm
@@ -1,9 +1,9 @@
 # -*-CPerl-*-
-# Last changed Time-stamp: <2015-06-29 15:49:33 mtw>
+# Last changed Time-stamp: <2015-10-23 16:41:47 mtw>
 
 package Bio::ViennaNGS::Feature;
 
-use version; our $VERSION = qv('0.15');
+use version; our $VERSION = qv('0.16_01');
 
 use Moose;
 with 'MooseX::Clone';
@@ -24,17 +24,68 @@ has 'score' => (
 		default => '0',
 		predicate => 'has_score',
 	       );
-
-sub set_Feature{
-  my ($self,$chromosome,$start,$end,$name,$score,$strand) = @_;
-  $self->chromosome($chromosome);
-  $self->start($start);
-  $self->end($end);
-  $self->name($name);
-  $self->score($score);
-  $self->strand($strand);
 }
 
 no Moose;
 
 1;
+
+__END__
+
+
+=head1 NAME
+
+Bio::ViennaNGS::Feature - A Moose wrapper for BED6-type genomic intervals.
+
+=head1 SYNOPSIS
+
+  use Bio::ViennaNGS::Feature;
+
+  my $expression = Bio::ViennaNGS::Feature->new(chromosome => "chr1",
+                                                start => "1200",
+                                                end => "4300",
+                                                strand => "+",
+                                                name => "foobar",
+                                                score => "100",
+                                               );
+
+=head1 DESCRIPTION
+
+This module provides an object-oriented interface for storing genomic
+intervals characterized via chromosome, start position, end position,
+name, score and strand. As such, it can be regarded a simple wrapper
+for BED6 elements.
+
+This class inherits from L<Bio::ViennaNGS::MinimalFeature> and is the
+base classs for L<Bio::ViennaNGS::FeatureChain>.
+
+=head1 SEE ALSO
+
+=over
+
+=item L<Bio::ViennaNGS>
+
+=item L<Bio::ViennaNGS::MinimalFeature>
+
+=item L<Bio::ViennaNGS::FeatureChain>
+
+=back
+
+=head1 AUTHORS
+
+Michael T. Wolfinger E<lt>michael@wolfinger.euE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2014-2015 Michael T. Wolfinger E<lt>michael@wolfinger.euE<gt>
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.10.0 or,
+at your option, any later version of Perl 5 you may have available.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+General Public License for more details.
+
+=cut
