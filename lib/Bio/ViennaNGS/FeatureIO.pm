70f1a4bcfc9f52aae334a7da5c4fc38bc43b8e64
Michael T. Wolfinger <michael@wolfinger.eu>
2015-10-27 16:44:21 +0100

    added POD

--- lib/Bio/ViennaNGS/FeatureIO.pm
+++ lib/Bio/ViennaNGS/FeatureIO.pm
@@ -1,5 +1,5 @@
 # -*-CPerl-*-
-# Last changed Time-stamp: <2015-10-22 13:01:02 mtw>
+# Last changed Time-stamp: <2015-10-27 16:39:10 mtw>
 package Bio::ViennaNGS::FeatureIO;
 
 use Moose;
@@ -8,8 +8,6 @@ use File::Slurp;
 use Bio::ViennaNGS::Bed;
 use Bio::ViennaNGS::Feature;
 use Bio::ViennaNGS::BedGraphEntry;
-
-#use Data::Dumper;
 
 has 'file' => ( # file path
 	       is => 'ro',
@@ -86,3 +84,90 @@ sub parse_bedgraph_file{
 no Moose;
 
 1;
+
+__END__
+
+=head1 NAME
+
+Bio::ViennaNGS::FeatureIO - Versatile I/O interface for Bio::ViennaNGS
+feature annotation classes
+
+=head1 SYNOPSIS
+
+  use Bio::ViennaNGS::FeatureIO;
+
+  # create a new object and parse the contents of a bedGraph file
+  my $obj = Bio::ViennaNGS::FeatureIO->new(file       => "file.bg,
+                                           filetype   => "BedGraph",
+                                           objecttype => "BedGraph",
+                                          );
+
+
+
+=head1 DESCRIPTION
+
+This module provides an object-oriented interface for easy
+input/output operations on common feature annotation file formats. It
+is - by design - a very generic module that stores all annotation data
+within the C<$self-E<gt>data> ArrayRef. C<$self-E<gt>objecttype>
+determines the type of elements in C<$self-E<gt>data>.
+
+Currently parsing of bedGraph files into an array of
+L<Bio::ViennaNGS::BedGraphEntry> objects is supported.
+
+=head1 METHODS
+
+=over
+
+=item parse_bedgraph_file
+
+Title : parse_gff
+
+Usage : C<$obj-E<gt>parse_bedgraph_file($bedgraph_file);>
+
+Function: Parses bedGraph coverage data into C<$self-E<gt>data>.
+
+Args : The full path to a bedGraph file
+
+Returns : Nothing.
+
+Notes : The bedGraph specification is available at
+        L<http://genome.ucsc.edu/goldenpath/help/bedgraph.html>.
+
+=back
+
+=head1 DEPENDENCIES
+
+=over
+
+=item L<Bio::ViennaNGS::Bed>
+
+=item L<Bio::ViennaNGS::Feature>
+
+=item L<Bio::ViennaNGS::BedGraphEntry>
+
+=item L<File::Slurp>
+
+=item L<Carp>
+
+=back
+
+=head1 AUTHORS
+
+Michael T. Wolfinger E<lt>michael@wolfinger.euE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2014-2015 Michael T. Wolfinger E<lt>michael@wolfinger.euE<gt>
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.10.0 or,
+at your option, any later version of Perl 5 you may have available.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+General Public License for more details.
+
+
+=cut
